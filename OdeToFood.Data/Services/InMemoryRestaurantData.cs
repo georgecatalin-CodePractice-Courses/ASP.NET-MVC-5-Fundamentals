73befd1647af727e3ef8d0920d8d1ca9a0138423
George Calin <george.calin@lacon.de>
2020-01-20 16:19:30 +0200

    Creating a new ASP.NET MVC application.

---
+++ OdeToFood.Data/Services/InMemoryRestaurantData.cs
@@ -0,0 +1,27 @@
+﻿using OdeToFood.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OdeToFood.Data.Services
+{
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        List<Restaurant> restaurants;
+
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant(){Id=1,Name="Scapino", Cuisine=CuisineType.Italian},
+                new Restaurant(){Id=2,Name="Mango Grove", Cuisine=CuisineType.Indian},
+                new Restaurant(){Id=3,Name="Nantes",Cuisine=CuisineType.French}
+            };
+        }
+        
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return restaurants.OrderBy(r => r.Name);
+        }
+    }
+}
