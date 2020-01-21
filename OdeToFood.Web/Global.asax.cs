262fd84a5403c00434e1f91073ec343d033ef203
George Calin <george.calin@lacon.de>
2020-01-21 16:49:10 +0200

    Installing and configuring WebApi.

--- OdeToFood.Web/Global.asax.cs
+++ OdeToFood.Web/Global.asax.cs
@@ -5,6 +5,8 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Web.Http;
+
 
 namespace OdeToFood.Web
 {
@@ -14,9 +16,10 @@ namespace OdeToFood.Web
         {
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            GlobalConfiguration.Configure(WebApiConfig.Register);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-            ContainerConfig.RegisterContainer();
+            ContainerConfig.RegisterContainer(GlobalConfiguration.Configuration);
         }
     }
 }
