8edaad3538a0d9c6f811e995c0e8ed852e7b1d56
George Calin <george.calin@lacon.de>
2020-01-22 14:09:34 +0200

    Following the POST-Redirect-GET Pattern.

--- OdeToFood.Web/Controllers/RestaurantsController.cs
+++ OdeToFood.Web/Controllers/RestaurantsController.cs
@@ -50,7 +50,7 @@ namespace OdeToFood.Web.Controllers
             if (ModelState.IsValid)
             {
                 db.Add(restaurant);
-                return View();
+                return RedirectToAction("Details", new { id = restaurant.Id });
             }
 
             return View();
