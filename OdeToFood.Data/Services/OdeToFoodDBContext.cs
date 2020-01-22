d9e03a97915cc09140c3aa5a0cdd970e31bcb32e
georgecatalin <george.calin@gmail.com>
2020-01-22 16:34:13 +0200

    Defining a DBContext.

---
+++ OdeToFood.Data/Services/OdeToFoodDBContext.cs
@@ -0,0 +1,17 @@
+﻿
+
+using OdeToFood.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OdeToFood.Data.Services
+{
+    class OdeToFoodDBContext : DbContext
+    {
+        public DbSet<Restaurant> Restaurants { get; set; }
+    }
+}
